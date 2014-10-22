f60d9474650bcaf9e83d08391910d20d7739ff9b
Mike Gibson <mike@rebrandsoftware.com>
2014-10-22 17:12:15 -0400

    Debug element

--- www/js/main.js
+++ www/js/main.js
@@ -45,6 +45,8 @@ var app = {
             FileIO.getB64FromFileURI(fullPath, function(b64) {
                 console.log("called back b64; " + b64.length);
                 var $el = ('#imgLoadPhotoB64');
+                console.log("Element: ");
+                console.log($el);
                 $el.attr('src', b64).load();
             });
         });
