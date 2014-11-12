aea2b35ddf0d403e770c1c2f0e6d5f438b5aa279
Mike Gibson <mike@rebrandsoftware.com>
2014-11-12 16:21:05 -0500

    Interchange Blob and Binary Array

--- www/js/fileIO.js
+++ www/js/fileIO.js
@@ -68,116 +68,34 @@ var FileIO = {
     },
     
     getBlobFromBase64: function (b64Data, contentType, sliceSize) { 
-        // console.log('[FILEIO]: getBlobFromBase64');
-        // var binary = atob(bs64data.split(',')[1]); 
-        // // atob() decode base64 data.. 
-        // console.log("[FILEIO]: got binary");
-        // var array = []; 
-        // for (var i = 0; i < binary.length; i++) { 
-            // array.push(binary.charCodeAt(i));   
-            // // convert to binary.. 
-        // } 
-        // console.log("[FILEIO]: pushed to array");
-        // var blob=null;
-        // var byteArrays = [new Uint8Array(array)];
-        // var contentType = 'image/jpeg';
-        // try{
-           // blob = new Blob(byteArrays, {type: contentType});
-        // }
-        // catch(e){
-            // // TypeError old chrome and FF
-            // window.BlobBuilder = window.BlobBuilder || 
-                                 // window.WebKitBlobBuilder || 
-                                 // window.MozBlobBuilder || 
-                                 // window.MSBlobBuilder;
-            // if(e.name == 'TypeError' && window.BlobBuilder){
-                // console.log("[FILEIO]: using blob builder");
-                // var bb = new BlobBuilder();
-                // bb.append(byteArrays);
-                // blob = bb.getBlob(contentType);
-            // }
-            // else if(e.name == "InvalidStateError"){
-                // // InvalidStateError (tested on FF13 WinXP)
-                // console.log("[FILEIO]: invalid state error");
-                // blob = new Blob(byteArrays, {type : contentType});
-            // }
-            // else{
-                // console.log("[FILEIO]: we're screwed");
-                // // We're screwed, blob constructor unsupported entirely   
-            // }
-        // }
-        // //var file = new Blob([new Uint8Array(array)], {type: 'image/jpeg'});    // create blob file.. 
-        // console.log('[FILEIO]: Got Blob');
-        // console.log(blob);
-        // return blob;
-        var b64DataOrig = b64Data;
-        if (b64Data.indexOf(",") > -1) {
-            b64Data = b64Data.split(',')[1];
-        }
-        contentType = contentType || 'image/jpeg';
-        sliceSize = sliceSize || 512;
+        console.log('[FILEIO]: getBlobFromBase64');
+        var binary = atob(bs64data.split(',')[1]); 
+        // atob() decode base64 data.. 
+        console.log("[FILEIO]: got binary");
+        var array = []; 
+        for (var i = 0; i < binary.length; i++) { 
+            array.push(binary.charCodeAt(i));   
+            // convert to binary.. 
+        } 
+        console.log("[FILEIO]: pushed to array");
         var blob=null;
-        var byteCharacters = atob(b64Data);
-        var byteArrays = [];
-    
-        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-            var slice = byteCharacters.slice(offset, offset + sliceSize);
-            console.log("Slice");
-            var byteNumbers = new Array(slice.length);
-            for (var i = 0; i < slice.length; i++) {
-                byteNumbers[i] = slice.charCodeAt(i);
-                
-            }
-    
-            var byteArray = new Uint8Array(byteNumbers);
-    
-            byteArrays.push(byteArray);
-        }
-        
-
-        
-        //var blob = new Blob(byteArrays, {type: contentType});
-        
+        var byteArrays = [new Uint8Array(array)];
+        var byteArray = new Uint8Array(array);
+        var binaryArray=byteArray.buffer;
+        var contentType = 'image/jpeg';
         try{
-           blob = new Blob( byteArrays, {type : contentType});
-           console.log("Try Blob");
-           return blob;
+           blob = new Blob(byteArrays, {type: contentType});
         }
         catch(e){
             // TypeError old chrome and FF
-            console.log("Calling Img Blob");
-            FileIO.b64toBlob(b64DataOrig, 
-                function(blob) {
-                    console.log("Success!");
-                    console.log(blob);
-                    return blob;
-                }, 
-                function() {
-                    console.log("Error");
-                    return null;
-            });
+            blob = binaryArray;
         }
+        //var file = new Blob([new Uint8Array(array)], {type: 'image/jpeg'});    // create blob file.. 
+        console.log('[FILEIO]: Got Blob');
+        console.log(blob);
+        return blob;
 
-    },
-        
-    b64toBlob: function(b64, onsuccess, onerror) {
-        var img = new Image();
-    
-        img.onerror = onerror;
-    
-        img.onload = function onload() {
-            console.log("loaded image")
-            var canvas = document.createElement('canvas');
-            canvas.width = img.width;
-            canvas.height = img.height;
-    
-            var ctx = canvas.getContext('2d');
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-            console.log("drew image");
-            canvas.toBlob(onsuccess);
-        };
-    
-        img.src = b64;
+
     },
 
     // simple error handler
